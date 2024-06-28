9009095a1b78fe57f1855e19f4253401b056717d
Ruzzuu <121614064+Ruzzuu@users.noreply.github.com>
2024-06-29 01:37:49 +0700

    Update handle_transaksi.sql

--- database/transaction/handle_transaksi.sql
+++ database/transaction/handle_transaksi.sql
@@ -18,21 +18,5 @@ BEGIN
     VALUES (p_jenis_sepatu_id, p_jumlah, v_total_biaya, @last_pesanan_id);  
 END;
 
-INSERT INTO jenis_sepatu (jenis_sepatu_id, jenis_sepatu_nama, jenis_sepatu_harga) VALUES
-('JS01', 'Sneakers', 250000),
-('JS02', 'Loafers', 300000),
-('JS03', 'Boots', 550000),
-('JS04', 'Derby', 400000),
-('JS05', 'High Heels', 350000),
-('JS06', 'Sandals', 150000),
-('JS07', 'Athletic', 450000),
-('JS08', 'Casual Shoes', 200000),
-('JS09', 'Formal Shoes', 600000),
-('JS10', 'Slippers', 100000);
-
-DROP PROCEDURE IF EXISTS AddDetailPesanan;
 CALL AddDetailPesanan('JS01', 2);
 CALL AddDetailPesanan('JS02', 3);
-
-SELECT * FROM pesanan;
-SELECT * FROM informasi_usaha;
